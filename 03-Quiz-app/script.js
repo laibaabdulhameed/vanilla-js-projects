ccc3c48640a5f90906faacc74bb1e1874f30de77
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-04-05 06:13:36 +0100

    feat: add popup into screen HTML

---
+++ 03-Quiz-app/script.js
@@ -0,0 +1,10 @@
+import questions from"./data.js";
+
+const card = document.querySelector('.quiz__card');
+const questionNumber = document.querySelector('.quiz__questionNumber');
+const question = document.querySelector('.quiz__question');
+const  btn = document.querySelectorAll('.quiz__btn');
+const  progressFill= document.querySelector('.quiz__progress-fill');
+const  result= document.querySelector('.quiz__result');
+const score = document.querySelector('.quiz__score');
+const restart = document.querySelector('.quiz__restart');
