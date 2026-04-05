23d6dc266f4edf4eef112d87aacf49abab6102da
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-04-05 06:39:56 +0100

    faet: add popup screen CSS styles

--- 03-Quiz-app/script.js
+++ 03-Quiz-app/script.js
@@ -1,10 +1,11 @@
-import questions from"./data.js";
+import questions from "./data.js";
 
+const popupButton = document.querySelector('.popup__btn');
 const card = document.querySelector('.quiz__card');
 const questionNumber = document.querySelector('.quiz__questionNumber');
 const question = document.querySelector('.quiz__question');
-const  btn = document.querySelectorAll('.quiz__btn');
-const  progressFill= document.querySelector('.quiz__progress-fill');
-const  result= document.querySelector('.quiz__result');
+const btn = document.querySelectorAll('.quiz__btn');
+const progressFill = document.querySelector('.quiz__progress-fill');
+const result = document.querySelector('.quiz__result');
 const score = document.querySelector('.quiz__score');
 const restart = document.querySelector('.quiz__restart');
