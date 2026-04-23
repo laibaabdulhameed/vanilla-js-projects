42da72256b8fe2dfee2115b07b1c7b97b4325e27
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-04-23 11:37:43 +0100

    add transaction function and renderSummary working

---
+++ 05-Expense-Tracker/script.js
@@ -0,0 +1,89 @@
+let addbtn = document.querySelector(".transaction__add");
+let addamount = document.querySelector("#amount");
+let addcategory = document.querySelector("#category");
+let adddate = document.querySelector("#date-label");
+// let income = document.querySelector('.Income')
+// let expense = document.querySelector('.Expense')
+// let totalBalance = document.querySelector('.totalbalance')
+
+// let transactions = [];
+addbtn.addEventListener('click', addTransactions);
+function addTransactions(e) {
+    e.preventDefault();
+    // collect
+    const addtype = document.querySelector('input[name="type"]:checked');
+    const amount = addamount.value;
+    const type = addtype.value;
+    const category = addcategory.value;
+    const date = adddate.value;
+    // validate
+    if (!amount) {
+        alert("please add the ammount")
+        return;
+    }
+    if (amount <= 0) {
+        alert("Ammount must no be in negative")
+        return;
+    }
+    if (!type) {
+        alert("please select the type")
+        return;
+    }
+    if (!date) {
+        alert("please select the date")
+        return;
+    }
+    // create
+    const newTransctions = {
+        id: Date.now(),
+        amount: Number(amount),
+        type: type,
+        category: category,
+        date: date
+    }
+    //  push
+    transactions.push(newTransctions);
+    saveToStorage();
+    renderSummary();
+    addamount.value = '';
+    adddate.value = '';
+    document.querySelectorAll('input[name="type"]').forEach(r => r.checked = false);
+}
+// save to local storage
+function saveToStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+// load from Storage
+function loadFromStorage() {
+    const saved = localStorage.getItem('transactions');
+
+    transactions = saved ? JSON.parse(saved) : [];
+}
+
+// declare empty 
+let income;
+let expense;
+let totalBalance;
+
+let transactions = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    // assign when DOM ready
+    income = document.querySelector('.Income');
+    expense = document.querySelector('.Expense');
+    totalBalance = document.querySelector('.totalbalance');
+
+    loadFromStorage();
+    renderSummary();
+});
+
+// render
+function renderSummary() {
+    const totalIncome = transactions.filter(t => t.type === 'income').reduce((sum, t) => sum + t.amount, 0)
+    const totalexpense = transactions.filter(t => t.type === 'expense').reduce((sum, t) => sum + t.amount, 0)
+    const balance = totalIncome - totalexpense;
+    income.textContent = `$${totalIncome}`;
+    expense.textContent = `$${totalexpense}`;
+    totalBalance.textContent = `$${balance}`;
+}
+
