3ecfe64fd0382e962b39aebb95e646361d70234d
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-05-08 08:32:00 +0100

    tip-calcultaor

---
+++ 00-mini-projects/06-Tip-Calculator/script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('#button');
+const total = document.querySelector('#total-amount');
+const perPerson = document.querySelector('#per-person');
+
+btn.addEventListener('click', () => {
+    const bill = Number(document.querySelector('#bill').value);
+    const tip = Number(document.querySelector('#tip').value);
+    const people = Number(document.querySelector('#people').value);
+    // conditions
+     if(bill<=0 || tip<=0 || people<=0){
+        alert("Please enter valid Number.")
+    }
+    let tipAmount = bill * tip / 100;
+    let totalAmount = tipAmount + bill;
+    total.textContent = `$${totalAmount} ` ;
+    // perPerson
+    let  per = totalAmount/people;
+    perPerson.textContent= `$${per}`;
+    
+    
+
+})
