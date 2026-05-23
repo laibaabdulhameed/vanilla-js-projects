f31069a4dea6b783ffe541ae917b8ab632e98299
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-05-23 05:55:34 +0100

    Add error handling for weather app

--- 00-mini-projects/18-weather-app/script.js
+++ 00-mini-projects/18-weather-app/script.js
@@ -7,17 +7,29 @@ const  humidity= document.querySelector('.humidity')
 const  speed= document.querySelector('.speed')
 
 async function getWeatherData() {
+   try{
     const city = areaname.value;
+    if(!city) return;
+    area.textContent = "Loading..."
     const response = await fetch(`https://wttr.in/${city}?format=j1`)
+    if(!response.ok)  throw new Error("city not found")
+      console.log(response.ok);
+      console.log(response.status);
+      
     const data = await response.json()
-      const  Condition = data.current_condition[0]
+    const  Condition = data.current_condition[0]
     const Area = data.nearest_area[0]
-    console.log(data);
     area.textContent = Area.areaName[0].value
     temp.textContent = `${Condition.FeelsLikeC}C`
     condition.textContent = Condition.weatherDesc[0].value
     humidity.textContent = `Humidity: ${Condition.humidity}%`
     
+   } catch(error){
+      area.textContent = "City not found"
+    temp.textContent = ""
+    condition.textContent =""
+    humidity.textContent = ""
+   }
     
 }
 
