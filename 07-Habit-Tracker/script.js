a3f8d14be6c0f3cc0ca34b0fc6f5e5b84d3ad070
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-06-13 20:42:38 +0100

    added a habit tracker

--- 07-Habit-Tracker/script.js
+++ 07-Habit-Tracker/script.js
@@ -34,14 +34,17 @@ function loadHabits() {
 function renderHabits() {
     displayData.innerHTML = "";
     data.forEach(habit => {
+        let today = new Date().toISOString().slice(0,10);
+        let isChecked = habit.completedDates.includes(today)? "Checked" : "";
+        let streak = getStreak(habit);
         displayData.innerHTML += `
           <div class="habit-card">
                 <div class="habit-info">
-                    <input type="checkbox" data-id="${habit.id}">
+                    <input type="checkbox" data-id="${habit.id}" ${isChecked}>
                     <span class="habit-name">${habit.name}</span>
                 </div>
                 <div class="habit-meta">
-                    <span class="streak">🔥 0-day streak</span>
+                    <span class="streak">🔥 ${streak}-day streak</span>
                     <button class="delete-btn" data-id="${habit.id}">Delete</button>
                 </div>
             </div>
@@ -49,10 +52,43 @@ function renderHabits() {
           `
     })
 }
-displayData.addEventListener('click',(e)=>{
- if(e.target.classList.contains('delete-btn')){
-   let id = Number(e.target.dataset.id);
-   data = data.filter(habit => habit.id!==id);
-   saveHabits();
-   renderHabits(); }
+displayData.addEventListener('click', (e) => {
+    console.log(e.target.tagName, e.target.type);
+    
+    if (e.target.classList.contains('delete-btn')) {
+        let id = Number(e.target.dataset.id);
+        data = data.filter(habit => habit.id !== id);
+        saveHabits();
+        renderHabits();
+    }
+    if (e.target.type === "checkbox") {
+        let id = Number(e.target.dataset.id);
+        let habit = data.find(h => h.id === id);
+        let today = new Date().toISOString().slice(0, 10);
+        if (habit.completedDates.includes(today)) {
+            habit.completedDates = habit.completedDates.filter(date => date !== today)
+        } else {
+            habit.completedDates.push(today);
+        }
+        saveHabits();
+        renderHabits();
+    }
+
 })
+function getStreak(habit){
+   let streak = 0;
+   let d = new Date();
+   while(true){
+    let dateStr = d.toISOString().slice(0, 10);
+    if(habit.completedDates.includes(dateStr)){
+        streak++;
+        d.setDate(d.getDate()-1)
+    }else{
+        break;
+    }
+   }
+  return streak;
+}
+
+
+
